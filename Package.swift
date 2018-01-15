c7ba4742345d4a0c220d62f6564f82e110a09b88
Nikolay Borysov <niks.borisov@gmail.com>
2018-01-15 14:14:38 +0200

    Update package.

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/vapor/vapor.git", .upToNextMajor(from: "2.1.0")),
         .package(url: "https://github.com/vapor/fluent-provider.git", .upToNextMajor(from: "1.2.0")),
-        .package(url: "https://github.com/czechboy0/Jay.git", .revision("1.0.1")),
+        .package(url: "https://github.com/czechboy0/Jay.git", .upToNextMajor(from: "1.0.1")),
     ],
     targets: [
         .target(
